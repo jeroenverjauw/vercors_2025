3b290e8360c30404cf4ac77b2aa599b175117478
JeroenVerjauw <jeroen.verjauw@iqm.fi>
2025-07-29 18:19:31 +0200

    postcard_and_paintings_update

--- pages/Souvenirs.py
+++ pages/Souvenirs.py
@@ -1,13 +1,13 @@
 import streamlit as st
 from pathlib import Path
-from streamlit_pdf_viewer import pdf_viewer
 
 from subprocess import run, PIPE
 
 
 st.title("Des Petites Souvenirs")
 
-
-pdf_viewer("data/pictures/extra/postkaart_voorkant.pdf") 
-pdf_viewer("data/pictures/extra/postkaart_achterkant.pdf") 
+st.image("data/pictures/extra/painting_1.jpg")
+st.image("data/pictures/extra/painting_2.jpg")
+st.image("data/pictures/extra/postkaart_voorkant.jpg") 
+st.image("data/pictures/extra/postkaart_achterkant.jpg") 
 st.image("data/pictures/Jeroen/IMG_4129.jpg")  
